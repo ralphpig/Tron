e98488c09f89ed2f25e20a57aa4e1acef25bfdae
Bryson Tang <brysontang@gmail.com>
2016-01-09 15:08:24 -0800

    Added Graphics (Not yet defined)

--- src/Window.java
+++ src/Window.java
@@ -8,8 +8,8 @@ public class Window {
     public static void create(){
         JFrame f = new JFrame("Tron");
         Draw g = new Draw();
-        f.add(g);
         f.setSize(800,800);
+        f.add(g);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }

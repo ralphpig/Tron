3daa7ca52c985f623e3a0900650869471cc39c1a
Bryson Tang <brysontang@gmail.com>
2016-01-09 14:31:03 -0800

    Added JFrame and JPanel

---
+++ src/Draw.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Bryson on 1/9/2016.
+ */
+public class Draw extends JPanel{
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+    }
+
+}
---
+++ src/Window.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+/**
+ * Created by Bryson on 1/9/2016.
+ */
+public class Window {
+
+    public static void create(){
+        JFrame f = new JFrame("Tron");
+        Draw g = new Draw();
+        f.add(g);
+        f.setSize(900,900);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
